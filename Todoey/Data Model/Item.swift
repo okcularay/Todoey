905c95510cc9fb00ca47b30c409176bd567126ee
Ayhan Okcular <okcular.ayhan@gmail.com>
2019-09-25 11:19:48 -0700

    Persisting data using remote

--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -2,14 +2,17 @@
 //  Item.swift
 //  Todoey
 //
-//  Created by Sky on 9/17/19.
+//  Created by Sky on 9/23/19.
 //  Copyright © 2019 Ayhan Okcular. All rights reserved.
 //
 
 import Foundation
+import RealmSwift
 
-class Item {
-    var title: String = ""
-    var done: Bool = false
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
     
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -7,63 +7,70 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = [Item]()
+    var toDoItems: Results<Item>?
     
-    let defaults = UserDefaults.standard
+    let realm = try! Realm()
+
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
         
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
+        tableView.separatorStyle = .none
         
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demorgon"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {itemArray = items}
     }
     
     //MARK TableView DataSource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return toDoItems?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let item = itemArray[indexPath.row]
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = item.title
         
+        //let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
-        //ternary operator
-        cell.accessoryType = item.done ? .checkmark : .none
-        
-       // if item.done == true {
-       //     cell.accessoryType = .checkmark
-       // }
-       // else {
-       //     cell.accessoryType = .none
-       // }
+        if let item = toDoItems?[indexPath.row] {
+            
+            cell.textLabel?.text = item.title
+            
+            //ternary operator
+            cell.accessoryType = item.done ? .checkmark : .none
+        }
+        else {
+            cell.textLabel?.text = "No items added"
+        }
         
         return cell
     }
     
     //MARK TableView Delegate Method
     
-    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        if let item = toDoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            }
+            catch {
+                print("Error saving done status \(error)")
+            }
+            
+        }
         
         tableView.reloadData()
         
@@ -73,15 +80,26 @@ class ToDoListViewController: UITableViewController {
     //Mark - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        print("\(String(describing: selectedCategory))")
         var textField = UITextField()
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //What will happen once the user clicks the add item button
-            let newItem = Item()
-            newItem.title = textField.text!
-            self.itemArray.append(newItem)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            self.tableView.reloadData() //Otherwise it will not show up on the screen
+            
+            if let currentCategory = self.selectedCategory {
+                do {
+                    try self.realm.write {
+                        let newItem = Item()
+                        newItem.title = textField.text!
+                        newItem.dateCreated = Date()
+                        currentCategory.items.append(newItem)
+                    }
+                }
+                catch {
+                    print("Error saving new items \(error)")
+                }
+            }
+            self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create new item"
@@ -89,6 +107,51 @@ class ToDoListViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func loadItems() {
+        
+        toDoItems = selectedCategory?.items.sorted(byKeyPath: "title", ascending: true)
+        
+        tableView.reloadData()
+        
+    }
+    
+//    override func updateModel(at indexPath: IndexPath) {
+//        if let item = todoItems?[indexPath.row] {
+//            do {
+//                try realm.write {
+//                    realm.delete(item)
+//                }
+//            } catch {
+//                print("Error deleting Item, \(error)")
+//            }
+//        }
+//    }
+    
+    
+}
+
+//Mark Search bar methods
+
+extension ToDoListViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        toDoItems = toDoItems?.filter("title CONTAINS[cd] %@", searchBar.text!).sorted(byKeyPath: "dateCreated", ascending: true)
+        
+        tableView.reloadData()
+
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
     }
 }
 
